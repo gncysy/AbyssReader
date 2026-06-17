3aa69114a6aab0e54115e8ec518151b5989ee716
gncysy <youxiang20110927@163.com>
2026-06-17 22:22:11 +0800

    Create SyncStatus.vue

---
+++ src/components/SyncStatus.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sync-status text-sm" v-if="lastSyncTime">
+    <span class="text-gray-400">📡 上次同步：{{ formatTime(lastSyncTime) }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useSyncStore } from '../stores/sync'
+import { storeToRefs } from 'pinia'
+
+const syncStore = useSyncStore()
+const { lastSyncTime } = storeToRefs(syncStore)
+
+const formatTime = (date: Date) => {
+  return date.toLocaleString()
+}
+</script>
