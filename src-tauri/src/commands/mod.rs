321f1bf3894d9fce7366eeb0d5d217e88d302dcf
gncysy <youxiang20110927@163.com>
2026-06-17 22:09:14 +0800

    Create mod.rs

---
+++ src-tauri/src/commands/mod.rs
@@ -0,0 +1,22 @@
+pub mod bookshelf;
+pub mod source;
+pub mod fetch_url;
+
+use tokio::sync::Mutex;
+use crate::storage::BookSource;
+
+pub struct AppState {
+    pub sources: Mutex<Vec<BookSource>>,
+}
+
+pub use bookshelf::{
+    get_bookshelf, add_to_bookshelf, remove_from_bookshelf,
+    update_reading_progress, get_reading_progress,
+};
+pub use source::{
+    get_book_sources, add_book_source, import_sources_from_url, upload_local_source,
+    delete_book_source, delete_book_sources, toggle_book_source,
+    test_book_source, test_all_sources, delete_failed_sources,
+    get_explore_categories,
+};
+pub use fetch_url::fetch_url;
